ac9c4a2af097317a6db6b54a490c383d91cd00f4
AidanDjalins <aidan.djalins@binus.ac.id>
2025-11-04 09:23:23 +0700

    Replaced human choice prompt with button selections. Added UI for choice and result display

--- script.js
+++ script.js
@@ -1,85 +1,93 @@
+playerDisplay = document.getElementById("playerDisplay");
+computerDisplay = document.getElementById("computerDisplay");
+finalResult = document.getElementById("finalResult");
 
-
-// Computer's choice
-function getComputerChoice() {
-    
-    let rand = Math.random();
-
-    if (rand <= 0.33) {
-        return "rock";
-    } else if (rand <= 0.66) {
-        return "paper";
-    } else {
-        return "scissors";
-    }
-}
-
-function getHumanChoice() {
-
-    let input = prompt("Rock, Paper, Scissors?")
-
-    if (input.toLowerCase() === "rock") {
-        return "rock";
-    } else if (input.toLowerCase() === "paper") {
-        return "paper";
-    } else {
-        return "scissors";
-    }
-}
-
-// Play the game!
-function playGame() {
-
-    // Declare the player and computer's score
+// Declare the player and computer's score
     let humanScore = 0;
     let computerScore = 0;
-
-    // Decide how many rounds will be played
-    let rounds = prompt("How many rounds do you wanna play?")
-
-    for(let i = 0; i < rounds; i++){
-        const humanSelection = getHumanChoice();
-        const computerSelection = getComputerChoice();
-
-        playRound(humanSelection, computerSelection);
-    }
-
-    // Function for one round
-    function playRound(humanChoice, computerChoice) {
     
-        let result = '';
-    
-        console.log("You chose: ", humanChoice)
-        console.log("CPU chose: ", computerChoice)
-    
-        if (humanChoice === computerChoice) {
-            result = "It's a draw!"
+// Computer's choice
+    function getComputerChoice() {
+        
+        let rand = Math.random();
+
+        if (rand <= 0.33) {
+            return "rock";
+        } else if (rand <= 0.66) {
+            return "paper";
         } else {
-            switch(humanChoice) {
-                case "rock":
-                    result = (computerChoice === "scissors") ? "You Win!" : "You Lose..";
-                    break;
-                case "paper":
-                    result = (computerChoice === "rock") ? "You Win!" : "You Lose..";
-                    break;
-                case "scissors":
-                    result = (computerChoice === "paper") ? "You Win!" : "You Lose..";
-                    break;
-            }
+            return "scissors";
         }
-    
-        switch(result) {
-            case "You Win!":
-                humanScore++;
-                break;
-            case "You Lose..":
-                computerScore++;
-                break;
-        }
-    
-        console.log(result);
-        console.log(`Score -> You: ${humanScore}, Computer: ${computerScore}`);
     }
+
+// Function for one round
+function playRound(humanChoice) {
+    
+    let result = '';
+    const computerChoice = getComputerChoice();
+    
+    console.log("You chose: ", humanChoice)
+    console.log("CPU chose: ", computerChoice)
+
+    if (humanChoice === computerChoice) {
+        result = "It's a draw!"
+    } else {
+        switch(humanChoice) {
+            case "rock":
+                result = (computerChoice === "scissors") ? "YOU WIN!" : "You Lose..";
+                break;
+            case "paper":
+                result = (computerChoice === "rock") ? "YOU WIN!" : "You Lose..";
+                break;
+            case "scissors":
+                result = (computerChoice === "paper") ? "YOU WIN!" : "You Lose..";
+                break;
+        }
+    }
+
+    playerDisplay.textContent = `YOU CHOSE: ${humanChoice}`;
+    computerDisplay.textContent = `CPU CHOSE: ${computerChoice}`;
+    finalResult.textContent = `${result}`
+
+    switch(result) {
+        case "YOU WIN!":
+            humanScore++;
+            break;
+        case "You Lose..":
+            computerScore++;
+            break;
+    }
+
+    console.log(result);
+    console.log(`Score -> You: ${humanScore}, Computer: ${computerScore}`);
 }
 
-console.log(playGame());
+// -- Unused -- 
+
+// // Decide how many rounds will be played
+// let rounds = prompt("How many rounds do you wanna play?")
+
+// for(let i = 0; i < rounds; i++){
+//     const humanSelection = getHumanChoice();
+//     const computerSelection = getComputerChoice();
+
+//     playRound(humanSelection, computerSelection);
+// }
+    
+
+// No need for the below function, the buttons will now serve as the human choice
+
+// function getHumanChoice() {
+
+//     let input = prompt("Rock, Paper, Scissors?")
+
+//     if (input.toLowerCase() === "rock") {
+//         return "rock";
+//     } else if (input.toLowerCase() === "paper") {
+//         return "paper";
+//     } else {
+//         return "scissors";
+//     }
+// }
+
+// Play the game!
